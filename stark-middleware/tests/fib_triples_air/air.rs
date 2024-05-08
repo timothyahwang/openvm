2a2c48937c0d86dcc731ddc1cb2a5c7da7e7c4ae
Osama Alkhodairy <osama.khodairy@hotmail.com>
2024-05-08 12:29:15 -0400

    test: added an integration test, fib_triples (#4)
    * test: added an integration test, fib_triples
    
    * test: modified fib_triples to change the public inputs

---
+++ stark-middleware/tests/fib_triples_air/air.rs
@@ -0,0 +1,52 @@
+use std::borrow::Borrow;
+
+use super::columns::{FibonacciCols, NUM_FIBONACCI_COLS};
+use afs_middleware::interaction::Chip;
+use p3_air::{Air, AirBuilder, AirBuilderWithPublicValues, BaseAir};
+use p3_field::Field;
+use p3_matrix::Matrix;
+
+pub struct FibonacciAir;
+
+// No interactions
+impl<F: Field> Chip<F> for FibonacciAir {}
+
+impl<F> BaseAir<F> for FibonacciAir {
+    fn width(&self) -> usize {
+        NUM_FIBONACCI_COLS
+    }
+}
+
+impl<AB: AirBuilderWithPublicValues> Air<AB> for FibonacciAir {
+    fn eval(&self, builder: &mut AB) {
+        let main = builder.main();
+        let pis = builder.public_values();
+
+        let a = pis[0];
+        let b = pis[1];
+        let x = pis[2];
+
+        let (local, next) = (main.row_slice(0), main.row_slice(1));
+        let local: &FibonacciCols<AB::Var> = (*local).borrow();
+        let next: &FibonacciCols<AB::Var> = (*next).borrow();
+
+        let mut when_first_row = builder.when_first_row();
+
+        when_first_row.assert_eq(local.left, a);
+        when_first_row.assert_eq(local.middle, b);
+        when_first_row.assert_eq(local.right, local.left + local.middle);
+
+        let mut when_transition = builder.when_transition();
+
+        // a' <- b
+        when_transition.assert_eq(local.middle, next.left);
+
+        // b' <- c
+        when_transition.assert_eq(local.right, next.middle);
+
+        // c' <- b + c
+        when_transition.assert_eq(local.middle + local.right, next.right);
+
+        builder.when_last_row().assert_eq(local.right, x);
+    }
+}
---
+++ stark-middleware/tests/fib_triples_air/columns.rs
@@ -0,0 +1,21 @@
+use p3_middleware_derive::AlignedBorrow;
+
+pub const NUM_FIBONACCI_COLS: usize = 3;
+
+#[repr(C)]
+#[derive(AlignedBorrow)]
+pub struct FibonacciCols<F> {
+    pub left: F,
+    pub middle: F,
+    pub right: F,
+}
+
+impl<F> FibonacciCols<F> {
+    pub const fn new(left: F, middle: F, right: F) -> FibonacciCols<F> {
+        FibonacciCols {
+            left,
+            middle,
+            right,
+        }
+    }
+}
