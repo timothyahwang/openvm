3fdbd122dbda153bccecf5082594c00be0fea74a
Ayush Shukla <shuklaayush247@gmail.com>
2024-05-31 18:49:12 +0200

    fix: add back imports (#38)

--- chips/src/lib.rs
+++ chips/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod keccak_permute;
 pub mod merkle_proof;
+pub mod page_controller;
+pub mod page_read;
 /// Chip to range check a value has less than a fixed number of bits
 pub mod range;
 pub mod range_gate;
