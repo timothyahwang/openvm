c2bd6ad167a08ee9c8cad0d389819cec65889890
Yu Jiang Tham <yoojbruin@gmail.com>
2024-06-21 10:55:48 -0400

    Add macros for testing

--- logical-interface/src/utils.rs
+++ logical-interface/src/utils.rs
@@ -95,3 +95,149 @@ pub fn fixed_bytes_to_field_vec(value: Vec<u8>) -> Vec<u32> {
         })
         .collect()
 }
+
+pub fn parse_literal_to_bytes(s: &str) -> Vec<u8> {
+    let s = stringify!(s);
+    if s.starts_with('"') {
+        // handle str
+        let s = s.strip_prefix('"').unwrap();
+        let s = s.strip_suffix('"').unwrap();
+        if s.starts_with("0x") {
+            let hex_str = s.strip_prefix("0x").unwrap();
+            hex::decode(hex_str).unwrap()
+        } else {
+            bytes_to_be_vec(s.as_bytes(), s.len())
+        }
+    } else {
+        // handle number
+        let num = s.parse::<u128>().unwrap();
+        let num = num.to_be_bytes().to_vec();
+        // remove leading zeros
+        num.iter().fold(Vec::<u8>::new(), |mut acc, &x| {
+            if x != 0 || !acc.is_empty() {
+                acc.push(x);
+            }
+            acc
+        })
+    }
+}
+
+#[macro_export]
+macro_rules! u8_vec {
+    ( $s:literal, $len:expr ) => {
+        {
+            let bytes_vec = u8_vec!($s);
+            let zeros_len = $len - bytes_vec.len();
+            let mut fixed_bytes = vec![0; zeros_len];
+            fixed_bytes.extend_from_slice(&bytes_vec);
+            fixed_bytes
+        }
+    };
+    ( $s:literal ) => {
+        {
+            let s = stringify!($s);
+            if s.starts_with('"') {
+                // handle str
+                let s = s.strip_prefix('"').unwrap();
+                let s = s.strip_suffix('"').unwrap();
+                if s.starts_with("0x") {
+                    let hex_str = s.strip_prefix("0x").unwrap();
+                    hex::decode(hex_str).unwrap()
+                } else {
+                    bytes_to_be_vec(s.as_bytes(), s.len())
+                }
+            } else {
+                // handle number
+                let num = s.parse::<u128>().unwrap();
+                let num = num.to_be_bytes().to_vec();
+                num.iter().fold(Vec::<u8>::new(), |mut acc, &x| {
+                    if x != 0 || !acc.is_empty() {
+                        acc.push(x);
+                    }
+                    acc
+                })
+            }
+        }
+    };
+    ( [$($x:expr),*], $len: expr ) => {
+        {
+            let bytes_vec = u8_vec!([$($x),*]);
+            let zeros_len = $len - bytes_vec.len();
+            let mut fixed_bytes = vec![0; zeros_len];
+            fixed_bytes.extend_from_slice(&bytes_vec);
+            fixed_bytes
+        }
+    };
+    ( [$($x:expr),*] ) => {
+        vec![$($x as u8),*]
+    };
+}
+
+#[macro_export]
+macro_rules! field_vec {
+    ( $s:literal, $len:expr ) => {
+        {
+            let v = field_vec!($s);
+            let zeros_len = $len - v.len();
+            let mut fixed_bytes = vec![0; zeros_len];
+            fixed_bytes.extend_from_slice(&v);
+            fixed_bytes
+        }
+    };
+    ( $s:literal ) => {
+        {
+            let v = u8_vec!($s);
+            v.chunks(2)
+                .map(|chunk| {
+                    let mut bytes = [0u8; 2];
+                    if chunk.len() == 1 {
+                        bytes[1] = chunk[0];
+                    } else {
+                        bytes.copy_from_slice(chunk);
+                    }
+                    u16::from_be_bytes(bytes) as u32
+                })
+                .collect::<Vec<u32>>()
+        }
+    };
+    ( [$($x:expr),*], $len: expr ) => {
+        {
+            let bytes_vec = field_vec!([$($x),*]);
+            let zeros_len = $len - bytes_vec.len();
+            let mut fixed_bytes = vec![0; zeros_len];
+            fixed_bytes.extend_from_slice(&bytes_vec);
+            fixed_bytes
+        }
+    };
+    ([$($x:expr),*]) => {
+        vec![$($x as u16 as u32),*]
+    };
+}
+
+#[test]
+pub fn test_macro_u8_vec() {
+    assert_eq!(u8_vec!("0x010101", 6), vec![0, 0, 0, 1, 1, 1]);
+    assert_eq!(u8_vec!("0x010101"), vec![1, 1, 1]);
+    assert_eq!(u8_vec!(1), vec![1]);
+    assert_eq!(u8_vec!([1, 2, 3], 5), vec![0, 0, 1, 2, 3]);
+    assert_eq!(u8_vec!("010101", 6), vec![48, 49, 48, 49, 48, 49]);
+    assert_eq!(u8_vec!([10, 10, 10]), vec![10, 10, 10]);
+    assert_eq!(u8_vec!(1, 2), vec![0, 1]);
+
+    let len = 4;
+    assert_eq!(u8_vec!("0x010101", len), vec![0, 1, 1, 1]);
+    let len = 10;
+    assert_eq!(u8_vec!("0x010101", len), vec![0, 0, 0, 0, 0, 0, 0, 1, 1, 1]);
+}
+
+#[test]
+pub fn test_macro_field_vec() {
+    assert_eq!(field_vec!(5), vec![5]);
+    assert_eq!(field_vec!("0x0101"), vec![257]);
+    assert_eq!(field_vec!("0x0101", 6), vec![0, 0, 0, 0, 0, 257]);
+    assert_eq!(field_vec!([10, 10, 10]), vec![10, 10, 10]);
+    assert_eq!(
+        field_vec!("0x003700370037", 8),
+        vec![0, 0, 0, 0, 0, 55, 55, 55]
+    );
+}
