67eacebd87410ab0de5765fcdcb0402cbf30ee6a
stephenh-axiom-xyz <stephenh@intrinsictech.xyz>
2024-10-17 11:32:15 -0400

    feat: AIR for LessThanCoreChip (#593)

--- vm/src/rv32im/new_lt/core.rs
+++ vm/src/rv32im/new_lt/core.rs
@@ -1,23 +1,28 @@
-use std::sync::Arc;
+use std::{
+    array,
+    borrow::{Borrow, BorrowMut},
+    sync::Arc,
+};
 
 use afs_derive::AlignedBorrow;
 use afs_primitives::xor::{bus::XorBus, lookup::XorLookupChip};
 use afs_stark_backend::{interaction::InteractionBuilder, rap::BaseAirWithPublicValues};
-use p3_air::{Air, BaseAir};
-use p3_field::{Field, PrimeField32};
+use p3_air::{AirBuilder, BaseAir};
+use p3_field::{AbstractField, Field, PrimeField32};
+use strum::IntoEnumIterator;
 
 use crate::{
     arch::{
         instructions::{LessThanOpcode, UsizeOpcode},
-        AdapterAirContext, AdapterRuntimeContext, Result, VmAdapterInterface, VmCoreAir,
-        VmCoreChip,
+        AdapterAirContext, AdapterRuntimeContext, MinimalInstruction, Result, VmAdapterInterface,
+        VmCoreAir, VmCoreChip,
     },
     system::program::Instruction,
 };
 
 #[repr(C)]
 #[derive(AlignedBorrow)]
-pub struct LessThanCols<T, const NUM_LIMBS: usize, const LIMB_BITS: usize> {
+pub struct LessThanCoreCols<T, const NUM_LIMBS: usize, const LIMB_BITS: usize> {
     pub b: [T; NUM_LIMBS],
     pub c: [T; NUM_LIMBS],
     pub cmp_result: T,
@@ -25,36 +30,33 @@ pub struct LessThanCols<T, const NUM_LIMBS: usize, const LIMB_BITS: usize> {
     pub opcode_slt_flag: T,
     pub opcode_sltu_flag: T,
 
-    pub x_sign: T,
-    pub y_sign: T,
+    // Most significant limb of b and c respectively as a field element, will be range
+    // checked to be within [-128, 127). Field xor_res is the result of (b_msb_f + 128)
+    // ^ (c_msb_f + 128) if signed and b_msb_f ^ c_msb_f else, used to range check
+    // b_msb_f and c_msb_f.
+    pub b_msb_f: T,
+    pub c_msb_f: T,
+    pub xor_res: T,
 
     // 1 at the most significant index i such that b[i] != c[i], otherwise 0. If such
     // an i exists, diff_val = c[i] - b[i]
-    pub diff_marker: [T; LIMB_BITS],
+    pub diff_marker: [T; NUM_LIMBS],
     pub diff_val: T,
 }
 
 #[derive(Copy, Clone, Debug)]
 pub struct LessThanCoreAir<const NUM_LIMBS: usize, const LIMB_BITS: usize> {
     pub bus: XorBus,
+    offset: usize,
 }
 
 impl<F: Field, const NUM_LIMBS: usize, const LIMB_BITS: usize> BaseAir<F>
     for LessThanCoreAir<NUM_LIMBS, LIMB_BITS>
 {
     fn width(&self) -> usize {
-        LessThanCols::<F, NUM_LIMBS, LIMB_BITS>::width()
+        LessThanCoreCols::<F, NUM_LIMBS, LIMB_BITS>::width()
     }
 }
-
-impl<AB: InteractionBuilder, const NUM_LIMBS: usize, const LIMB_BITS: usize> Air<AB>
-    for LessThanCoreAir<NUM_LIMBS, LIMB_BITS>
-{
-    fn eval(&self, _builder: &mut AB) {
-        todo!();
-    }
-}
-
 impl<F: Field, const NUM_LIMBS: usize, const LIMB_BITS: usize> BaseAirWithPublicValues<F>
     for LessThanCoreAir<NUM_LIMBS, LIMB_BITS>
 {
@@ -65,22 +67,111 @@ impl<AB, I, const NUM_LIMBS: usize, const LIMB_BITS: usize> VmCoreAir<AB, I>
 where
     AB: InteractionBuilder,
     I: VmAdapterInterface<AB::Expr>,
+    I::Reads: From<[[AB::Expr; NUM_LIMBS]; 2]>,
+    I::Writes: From<[[AB::Expr; NUM_LIMBS]; 1]>,
+    I::ProcessedInstruction: From<MinimalInstruction<AB::Expr>>,
 {
     fn eval(
         &self,
-        _builder: &mut AB,
-        _local_core: &[AB::Var],
+        builder: &mut AB,
+        local_core: &[AB::Var],
         _from_pc: AB::Var,
     ) -> AdapterAirContext<AB::Expr, I> {
-        todo!()
+        let cols: &LessThanCoreCols<_, NUM_LIMBS, LIMB_BITS> = local_core.borrow();
+        let flags = [cols.opcode_slt_flag, cols.opcode_sltu_flag];
+
+        let is_valid = flags.iter().fold(AB::Expr::zero(), |acc, &flag| {
+            builder.assert_bool(flag);
+            acc + flag.into()
+        });
+        builder.assert_bool(is_valid.clone());
+
+        let b = &cols.b;
+        let c = &cols.c;
+        let marker = &cols.diff_marker;
+        let mut prefix_sum = AB::Expr::zero();
+
+        let b_diff = b[NUM_LIMBS - 1] - cols.b_msb_f;
+        let c_diff = c[NUM_LIMBS - 1] - cols.c_msb_f;
+        builder
+            .assert_zero(b_diff.clone() * (AB::Expr::from_canonical_u32(1 << LIMB_BITS) - b_diff));
+        builder
+            .assert_zero(c_diff.clone() * (AB::Expr::from_canonical_u32(1 << LIMB_BITS) - c_diff));
+
+        for i in (0..NUM_LIMBS).rev() {
+            let diff = (if i == NUM_LIMBS - 1 {
+                cols.c_msb_f - cols.b_msb_f
+            } else {
+                c[i] - b[i]
+            }) * (AB::Expr::from_canonical_u8(2) * cols.cmp_result - AB::Expr::one());
+            prefix_sum += marker[i].into();
+            builder.assert_bool(marker[i]);
+            builder.assert_zero((AB::Expr::one() - prefix_sum.clone()) * diff.clone());
+            builder.when(marker[i]).assert_eq(cols.diff_val, diff);
+        }
+
+        builder.assert_bool(prefix_sum.clone());
+        builder
+            .when(AB::Expr::one() - prefix_sum)
+            .assert_zero(cols.cmp_result);
+
+        self.bus
+            .send(
+                cols.b_msb_f
+                    + AB::Expr::from_canonical_u32(1 << (LIMB_BITS - 1)) * cols.opcode_slt_flag,
+                cols.c_msb_f
+                    + AB::Expr::from_canonical_u32(1 << (LIMB_BITS - 1)) * cols.opcode_slt_flag,
+                cols.xor_res,
+            )
+            .eval(builder, is_valid.clone());
+        self.bus
+            .send(
+                cols.diff_val - AB::Expr::one(),
+                cols.diff_val - AB::Expr::one(),
+                AB::F::zero(),
+            )
+            .eval(builder, is_valid.clone());
+
+        let expected_opcode = flags
+            .iter()
+            .zip(LessThanOpcode::iter())
+            .fold(AB::Expr::zero(), |acc, (flag, opcode)| {
+                acc + (*flag).into() * AB::Expr::from_canonical_u8(opcode as u8)
+            })
+            + AB::Expr::from_canonical_usize(self.offset);
+        let mut a: [AB::Expr; NUM_LIMBS] = array::from_fn(|_| AB::Expr::zero());
+        a[0] = cols.cmp_result.into();
+
+        AdapterAirContext {
+            to_pc: None,
+            reads: [cols.b.map(Into::into), cols.c.map(Into::into)].into(),
+            writes: [a].into(),
+            instruction: MinimalInstruction {
+                is_valid,
+                opcode: expected_opcode,
+            }
+            .into(),
+        }
     }
+}
+
+#[derive(Clone, Debug)]
+pub struct LessThanCoreRecord<T, const NUM_LIMBS: usize, const LIMB_BITS: usize> {
+    pub opcode: LessThanOpcode,
+    pub b: [T; NUM_LIMBS],
+    pub c: [T; NUM_LIMBS],
+    pub cmp_result: T,
+    pub b_msb_f: T,
+    pub c_msb_f: T,
+    pub xor_res: T,
+    pub diff_val: T,
+    pub diff_idx: usize,
 }
 
 #[derive(Debug)]
 pub struct LessThanCoreChip<const NUM_LIMBS: usize, const LIMB_BITS: usize> {
     pub air: LessThanCoreAir<NUM_LIMBS, LIMB_BITS>,
     pub xor_lookup_chip: Arc<XorLookupChip<LIMB_BITS>>,
-    offset: usize,
 }
 
 impl<const NUM_LIMBS: usize, const LIMB_BITS: usize> LessThanCoreChip<NUM_LIMBS, LIMB_BITS> {
@@ -88,9 +179,9 @@ impl<const NUM_LIMBS: usize, const LIMB_BITS: usize> LessThanCoreChip<NUM_LIMBS,
         Self {
             air: LessThanCoreAir {
                 bus: xor_lookup_chip.bus(),
+                offset,
             },
             xor_lookup_chip,
-            offset,
         }
     }
 }
@@ -101,8 +192,7 @@ where
     I::Reads: Into<[[F; NUM_LIMBS]; 2]>,
     I::Writes: From<[[F; NUM_LIMBS]; 1]>,
 {
-    // TODO: update for trace generation
-    type Record = u32;
+    type Record = LessThanCoreRecord<F, NUM_LIMBS, LIMB_BITS>;
     type Air = LessThanCoreAir<NUM_LIMBS, LIMB_BITS>;
 
     #[allow(clippy::type_complexity)]
@@ -113,32 +203,93 @@ where
         reads: I::Reads,
     ) -> Result<(AdapterRuntimeContext<F, I>, Self::Record)> {
         let Instruction { opcode, .. } = instruction;
-        let local_opcode_index = LessThanOpcode::from_usize(opcode - self.offset);
+        let less_than_opcode = LessThanOpcode::from_usize(opcode - self.air.offset);
 
         let data: [[F; NUM_LIMBS]; 2] = reads.into();
-        let x = data[0].map(|x| x.as_canonical_u32());
-        let y = data[1].map(|y| y.as_canonical_u32());
-        let (cmp_result, _diff_idx, _x_sign, _y_sign) =
-            solve_less_than::<NUM_LIMBS, LIMB_BITS>(local_opcode_index, &x, &y);
+        let b = data[0].map(|x| x.as_canonical_u32());
+        let c = data[1].map(|y| y.as_canonical_u32());
+        let (cmp_result, diff_idx, b_sign, c_sign) =
+            solve_less_than::<NUM_LIMBS, LIMB_BITS>(less_than_opcode, &b, &c);
+
+        // xor_res is the result of (b_msb_f + 128) ^ (c_msb_f + 128) if signed,
+        // b_msb_f ^ c_msb_f if not
+        let (b_msb_f, b_msb_xor) = if b_sign {
+            (
+                -F::from_canonical_u32((1 << LIMB_BITS) - b[NUM_LIMBS - 1]),
+                b[NUM_LIMBS - 1] - (1 << (LIMB_BITS - 1)),
+            )
+        } else {
+            (
+                F::from_canonical_u32(b[NUM_LIMBS - 1]),
+                b[NUM_LIMBS - 1]
+                    + (((less_than_opcode == LessThanOpcode::SLT) as u32) << (LIMB_BITS - 1)),
+            )
+        };
+        let (c_msb_f, c_msb_xor) = if c_sign {
+            (
+                -F::from_canonical_u32((1 << LIMB_BITS) - c[NUM_LIMBS - 1]),
+                c[NUM_LIMBS - 1] - (1 << (LIMB_BITS - 1)),
+            )
+        } else {
+            (
+                F::from_canonical_u32(c[NUM_LIMBS - 1]),
+                c[NUM_LIMBS - 1]
+                    + (((less_than_opcode == LessThanOpcode::SLT) as u32) << (LIMB_BITS - 1)),
+            )
+        };
+        let xor_res = self.xor_lookup_chip.request(b_msb_xor, c_msb_xor);
+
+        let diff_val = if diff_idx == (NUM_LIMBS - 1) {
+            if cmp_result {
+                c_msb_f - b_msb_f
+            } else {
+                b_msb_f - c_msb_f
+            }
+            .as_canonical_u32()
+        } else if cmp_result {
+            c[diff_idx] - b[diff_idx]
+        } else {
+            b[diff_idx] - c[diff_idx]
+        };
+
+        // TODO: update XorLookupChip to either be BitwiseOperation or range check
+        self.xor_lookup_chip.request(diff_val - 1, diff_val - 1);
 
         let mut writes = [0u32; NUM_LIMBS];
         writes[0] = cmp_result as u32;
 
-        // Core doesn't modify PC directly, so we let Adapter handle the increment
         let output = AdapterRuntimeContext::without_pc([writes.map(F::from_canonical_u32)]);
+        let record = LessThanCoreRecord {
+            opcode: less_than_opcode,
+            b: b.map(F::from_canonical_u32),
+            c: c.map(F::from_canonical_u32),
+            cmp_result: F::from_bool(cmp_result),
+            b_msb_f,
+            c_msb_f,
+            xor_res: F::from_canonical_u32(xor_res),
+            diff_val: F::from_canonical_u32(diff_val),
+            diff_idx,
+        };
 
-        // TODO: send XorLookupChip requests
-        // TODO: create Record and return
-
-        Ok((output, 0))
+        Ok((output, record))
     }
 
-    fn get_opcode_name(&self, _opcode: usize) -> String {
-        todo!()
+    fn get_opcode_name(&self, opcode: usize) -> String {
+        format!("{:?}", LessThanOpcode::from_usize(opcode - self.air.offset))
     }
 
-    fn generate_trace_row(&self, _row_slice: &mut [F], _record: Self::Record) {
-        todo!()
+    fn generate_trace_row(&self, row_slice: &mut [F], record: Self::Record) {
+        let row_slice: &mut LessThanCoreCols<_, NUM_LIMBS, LIMB_BITS> = row_slice.borrow_mut();
+        row_slice.b = record.b;
+        row_slice.c = record.c;
+        row_slice.cmp_result = record.cmp_result;
+        row_slice.b_msb_f = record.b_msb_f;
+        row_slice.c_msb_f = record.c_msb_f;
+        row_slice.xor_res = record.xor_res;
+        row_slice.diff_val = record.diff_val;
+        row_slice.opcode_slt_flag = F::from_bool(record.opcode == LessThanOpcode::SLT);
+        row_slice.opcode_sltu_flag = F::from_bool(record.opcode == LessThanOpcode::SLTU);
+        row_slice.diff_marker = array::from_fn(|i| F::from_bool(i == record.diff_idx));
     }
 
     fn air(&self) -> &Self::Air {
@@ -159,5 +310,5 @@ pub(super) fn solve_less_than<const NUM_LIMBS: usize, const LIMB_BITS: usize>(
             return ((x[i] < y[i]) ^ x_sign ^ y_sign, i, x_sign, y_sign);
         }
     }
-    (false, 0, x_sign, y_sign)
+    (false, NUM_LIMBS, x_sign, y_sign)
 }
--- vm/src/rv32im/new_lt/tests.rs
+++ vm/src/rv32im/new_lt/tests.rs
@@ -1,15 +1,159 @@
-use super::core::solve_less_than;
-use crate::arch::instructions::LessThanOpcode;
+use std::{array, sync::Arc};
 
-const RV32_NUM_LIMBS: usize = 4;
-const RV32_LIMB_BITS: usize = 8;
+use afs_primitives::xor::lookup::XorLookupChip;
+use ax_sdk::utils::create_seeded_rng;
+use p3_baby_bear::BabyBear;
+use p3_field::AbstractField;
+use rand::{rngs::StdRng, Rng};
+
+use super::{core::solve_less_than, LessThanCoreChip, Rv32LessThanChip};
+use crate::{
+    arch::{
+        instructions::LessThanOpcode,
+        testing::{memory::gen_pointer, VmChipTestBuilder},
+        InstructionExecutor,
+    },
+    kernels::core::BYTE_XOR_BUS,
+    rv32im::adapters::{
+        Rv32BaseAluAdapterChip, RV32_CELL_BITS, RV32_REGISTER_NUM_LANES, RV_IS_TYPE_IMM_BITS,
+    },
+    system::program::Instruction,
+};
+
+type F = BabyBear;
+
+///////////////////////////////////////////////////////////////////////////////////////
+/// POSITIVE TESTS
+///
+/// Randomly generate computations and execute, ensuring that the generated trace
+/// passes all constraints.
+///////////////////////////////////////////////////////////////////////////////////////
+
+fn generate_long_number<const NUM_LIMBS: usize, const LIMB_BITS: usize>(
+    rng: &mut StdRng,
+) -> [u32; NUM_LIMBS] {
+    array::from_fn(|_| rng.gen_range(0..(1 << LIMB_BITS)))
+}
+
+fn generate_rv32_immediate(rng: &mut StdRng) -> (Option<usize>, [u32; RV32_REGISTER_NUM_LANES]) {
+    let mut imm: u32 = rng.gen_range(0..(1 << RV_IS_TYPE_IMM_BITS));
+    if (imm & 0x800) != 0 {
+        imm |= !0xFFF
+    }
+    (
+        Some((imm & 0xFFFFFF) as usize),
+        [
+            imm as u8,
+            (imm >> 8) as u8,
+            (imm >> 16) as u8,
+            (imm >> 16) as u8,
+        ]
+        .map(|x| x as u32),
+    )
+}
+
+#[allow(clippy::too_many_arguments)]
+fn run_rv32_lt_rand_write_execute<E: InstructionExecutor<F>>(
+    tester: &mut VmChipTestBuilder<F>,
+    chip: &mut E,
+    opcode: LessThanOpcode,
+    b: [u32; RV32_REGISTER_NUM_LANES],
+    c: [u32; RV32_REGISTER_NUM_LANES],
+    c_imm: Option<usize>,
+    rng: &mut StdRng,
+) {
+    let is_imm = c_imm.is_some();
+
+    let rs1 = gen_pointer(rng, 32);
+    let rs2 = c_imm.unwrap_or_else(|| gen_pointer(rng, 32));
+    let rd = gen_pointer(rng, 32);
+
+    tester.write::<RV32_REGISTER_NUM_LANES>(1, rs1, b.map(F::from_canonical_u32));
+    if !is_imm {
+        tester.write::<RV32_REGISTER_NUM_LANES>(1, rs2, c.map(F::from_canonical_u32));
+    }
+
+    let (cmp, _, _, _) = solve_less_than::<RV32_REGISTER_NUM_LANES, RV32_CELL_BITS>(opcode, &b, &c);
+    tester.execute(
+        chip,
+        Instruction::from_usize(
+            opcode as usize,
+            [rd, rs1, rs2, 1, if is_imm { 0 } else { 1 }],
+        ),
+    );
+    let mut a = [0; RV32_REGISTER_NUM_LANES];
+    a[0] = cmp as u32;
+
+    assert_eq!(
+        a.map(F::from_canonical_u32),
+        tester.read::<RV32_REGISTER_NUM_LANES>(1, rd)
+    );
+}
+
+fn run_rv32_lt_rand_test(opcode: LessThanOpcode, num_ops: usize) {
+    let mut rng = create_seeded_rng();
+
+    let xor_lookup_chip = Arc::new(XorLookupChip::<RV32_CELL_BITS>::new(BYTE_XOR_BUS));
+    let mut tester = VmChipTestBuilder::default();
+    let mut chip = Rv32LessThanChip::<F>::new(
+        Rv32BaseAluAdapterChip::new(
+            tester.execution_bus(),
+            tester.program_bus(),
+            tester.memory_controller(),
+        ),
+        LessThanCoreChip::new(xor_lookup_chip.clone(), 0),
+        tester.memory_controller(),
+    );
+
+    for _ in 0..num_ops {
+        let b = generate_long_number::<RV32_REGISTER_NUM_LANES, RV32_CELL_BITS>(&mut rng);
+        let (c_imm, c) = if rng.gen_bool(0.5) {
+            (
+                None,
+                generate_long_number::<RV32_REGISTER_NUM_LANES, RV32_CELL_BITS>(&mut rng),
+            )
+        } else {
+            generate_rv32_immediate(&mut rng)
+        };
+        run_rv32_lt_rand_write_execute(&mut tester, &mut chip, opcode, b, c, c_imm, &mut rng);
+    }
+
+    let tester = tester.build().load(chip).load(xor_lookup_chip).finalize();
+    tester.simple_test().expect("Verification failed");
+}
+
+#[test]
+fn rv32_slt_rand_test() {
+    run_rv32_lt_rand_test(LessThanOpcode::SLT, 12);
+}
+
+#[test]
+fn rv32_sltu_rand_test() {
+    run_rv32_lt_rand_test(LessThanOpcode::SLTU, 12);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+/// NEGATIVE TESTS
+///
+/// Given a fake trace of a single operation, setup a chip and run the test. We replace
+/// the write part of the trace and check that the core chip throws the expected error.
+/// A dummy adapter is used so memory interactions don't indirectly cause false passes.
+///////////////////////////////////////////////////////////////////////////////////////
+
+// TODO: write negative tests
+
+///////////////////////////////////////////////////////////////////////////////////////
+/// SANITY TESTS
+///
+/// Ensure that solve functions produce the correct results.
+///////////////////////////////////////////////////////////////////////////////////////
 
 #[test]
 fn solve_sltu_sanity_test() {
-    let x: [u32; RV32_NUM_LIMBS] = [145, 34, 25, 205];
-    let y: [u32; RV32_NUM_LIMBS] = [73, 35, 25, 205];
+    let x: [u32; RV32_REGISTER_NUM_LANES] = [145, 34, 25, 205];
+    let y: [u32; RV32_REGISTER_NUM_LANES] = [73, 35, 25, 205];
     let (cmp_result, diff_idx, x_sign, y_sign) =
-        solve_less_than::<RV32_NUM_LIMBS, RV32_LIMB_BITS>(LessThanOpcode::SLTU, &x, &y);
+        solve_less_than::<RV32_REGISTER_NUM_LANES, RV32_CELL_BITS>(LessThanOpcode::SLTU, &x, &y);
     assert!(cmp_result);
     assert_eq!(diff_idx, 1);
     assert!(!x_sign); // unsigned
@@ -18,10 +162,10 @@ fn solve_sltu_sanity_test() {
 
 #[test]
 fn solve_slt_same_sign_sanity_test() {
-    let x: [u32; RV32_NUM_LIMBS] = [145, 34, 25, 205];
-    let y: [u32; RV32_NUM_LIMBS] = [73, 35, 25, 205];
+    let x: [u32; RV32_REGISTER_NUM_LANES] = [145, 34, 25, 205];
+    let y: [u32; RV32_REGISTER_NUM_LANES] = [73, 35, 25, 205];
     let (cmp_result, diff_idx, x_sign, y_sign) =
-        solve_less_than::<RV32_NUM_LIMBS, RV32_LIMB_BITS>(LessThanOpcode::SLT, &x, &y);
+        solve_less_than::<RV32_REGISTER_NUM_LANES, RV32_CELL_BITS>(LessThanOpcode::SLT, &x, &y);
     assert!(cmp_result);
     assert_eq!(diff_idx, 1);
     assert!(x_sign); // negative
@@ -30,10 +174,10 @@ fn solve_slt_same_sign_sanity_test() {
 
 #[test]
 fn solve_slt_diff_sign_sanity_test() {
-    let x: [u32; RV32_NUM_LIMBS] = [45, 35, 25, 55];
-    let y: [u32; RV32_NUM_LIMBS] = [173, 34, 25, 205];
+    let x: [u32; RV32_REGISTER_NUM_LANES] = [45, 35, 25, 55];
+    let y: [u32; RV32_REGISTER_NUM_LANES] = [173, 34, 25, 205];
     let (cmp_result, diff_idx, x_sign, y_sign) =
-        solve_less_than::<RV32_NUM_LIMBS, RV32_LIMB_BITS>(LessThanOpcode::SLT, &x, &y);
+        solve_less_than::<RV32_REGISTER_NUM_LANES, RV32_CELL_BITS>(LessThanOpcode::SLT, &x, &y);
     assert!(!cmp_result);
     assert_eq!(diff_idx, 3);
     assert!(!x_sign); // positive
