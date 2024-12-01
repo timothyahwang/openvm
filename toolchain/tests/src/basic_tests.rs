53e9f8a351a21dbbb2c0c2642a8d45bfc111dc41
Yi Sun <yi-sun@users.noreply.github.com>
2024-11-30 21:07:49 -0500

    chore: consolidate keccak transpiler folder (#899)
    * chore: consolidate keccak transpiler folder
    
    * fix: lint

--- toolchain/tests/src/basic_tests.rs
+++ toolchain/tests/src/basic_tests.rs
@@ -4,9 +4,7 @@ use ax_stark_sdk::ax_stark_backend::p3_field::AbstractField;
 use axvm_bigint_circuit::Int256Rv32Config;
 use axvm_circuit::{
     arch::{
-        hasher::poseidon2::vm_poseidon2_hasher,
-        instructions::exe::AxVmExe,
-        new_vm::{self, VmExecutor},
+        hasher::poseidon2::vm_poseidon2_hasher, instructions::exe::AxVmExe, new_vm::VmExecutor,
     },
     system::memory::tree::public_values::UserPublicValuesProof,
     utils::new_air_test_with_min_segments,
