6b4cddd346cf397f03697e6a00d0b862c52124d4
Yi Sun <yi-sun@users.noreply.github.com>
2024-12-01 23:03:37 -0500

    fix: CI (#906)
    * fix: CI
    
    * fix: fib_e2e updates
    
    * fix: fib_e2e updates
    
    * fix: fib_e2e updates
    
    * fix: fib_e2e updates

--- benchmarks/src/bin/fib_e2e.rs
+++ benchmarks/src/bin/fib_e2e.rs
@@ -1,18 +1,24 @@
+use std::rc::Rc;
+
 use ax_stark_sdk::{
     bench::run_with_metric_collection,
     config::fri_params::standard_fri_params_with_100_bits_conjectured_security,
     p3_baby_bear::BabyBear,
 };
 use axvm_benchmarks::utils::{build_bench_program, BenchmarkCli};
-use axvm_circuit::arch::instructions::program::DEFAULT_MAX_NUM_PUBLIC_VALUES;
+use axvm_circuit::arch::instructions::{exe::AxVmExe, program::DEFAULT_MAX_NUM_PUBLIC_VALUES};
 use axvm_native_compiler::{conversion::CompilerOptions, prelude::Witness};
 use axvm_recursion::witness::Witnessable;
+use axvm_rv32im_circuit::Rv32ImConfig;
+use axvm_rv32im_transpiler::{
+    Rv32ITranspilerExtension, Rv32IoTranspilerExtension, Rv32MTranspilerExtension,
+};
 use axvm_sdk::{
     config::{AggConfig, AppConfig},
     e2e_prover::{commit_app_exe, generate_leaf_committed_exe, E2EStarkProver},
     keygen::{AggProvingKey, AppProvingKey},
 };
-use axvm_transpiler::axvm_platform::bincode;
+use axvm_transpiler::{axvm_platform::bincode, transpiler::Transpiler, FromElf};
 use clap::Parser;
 use eyre::Result;
 use p3_field::AbstractField;
@@ -53,8 +59,16 @@ async fn main() -> Result<()> {
     };
 
     let app_pk = AppProvingKey::keygen(app_config.clone());
-    let agg_pk = AggProvinigKey::keygen(agg_config.clone());
-    let app_committed_exe = commit_app_exe(app_config, build_bench_program("fibonacci").unwrap());
+    let agg_pk = AggProvingKey::keygen(agg_config.clone());
+    let elf = build_bench_program("fibonacci").unwrap();
+    let exe = AxVmExe::from_elf(
+        elf,
+        Transpiler::<BabyBear>::default()
+            .with_processor(Rc::new(Rv32ITranspilerExtension))
+            .with_processor(Rc::new(Rv32MTranspilerExtension))
+            .with_processor(Rc::new(Rv32IoTranspilerExtension)),
+    );
+    let app_committed_exe = commit_app_exe(app_config, exe);
     let leaf_committed_exe = generate_leaf_committed_exe(agg_config, &app_pk);
 
     let prover = E2EStarkProver::new(app_pk, agg_pk, app_committed_exe, leaf_committed_exe, 2, 2);
