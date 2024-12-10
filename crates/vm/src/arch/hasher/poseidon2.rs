edfa70217492be538ffeb101b2ba9ff4004f155e
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-12-10 16:21:25 -0500

    chore: make stark-sdk optional dep of vm (#992)
    * chore: make stark-sdk optional dep of vm
    
    * fix: vm needs p3-baby-bear always for system

--- crates/vm/src/arch/hasher/poseidon2.rs
+++ crates/vm/src/arch/hasher/poseidon2.rs
@@ -5,7 +5,7 @@ use std::{
 
 use ax_poseidon2_air::{p3_poseidon2::ExternalLayerConstants, p3_symmetric::Permutation};
 use ax_stark_backend::p3_field::{AbstractField, PrimeField32};
-use ax_stark_sdk::p3_baby_bear::{BabyBear, Poseidon2BabyBear};
+use p3_baby_bear::{BabyBear, Poseidon2BabyBear};
 
 use crate::{
     arch::{hasher::Hasher, vm_poseidon2_config, POSEIDON2_WIDTH},
