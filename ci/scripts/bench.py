2960ee8662fee8dda169c232aaa45d2f27b5c183
Lun-Kai Hsu <kaikaikai1219@gmail.com>
2024-12-11 11:23:43 -0800

    [feat] reth benchmark (#968)
    * test
    
    * test
    
    * tesT
    
    * update
    
    * r8g doesnt work
    
    * update
    
    * fix
    
    * nightly?
    
    * url
    
    * debug
    
    * fix
    
    * rm colon
    
    * rm one input
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * fix
    
    * cleanup
    
    * chore: use cargo build to hide rpc
    
    * fix: trusted setup dir
    
    * chore: upload metrics to s3
    
    * some refactor, and add to reth
    
    * fix
    
    * debug
    
    * fix
    
    * fix
    
    * gh page
    
    * fix
    
    * fix
    
    * gh page branch cannot use script...
    
    * fix
    
    * restore
    
    * fix
    
    * more cleanup
    
    * fix working dir
    
    * fix
    
    * debug
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * address comments
    
    * test
    
    * test
    
    * test
    
    * simplify metric name and path
    
    * change from jpw/reth-bench
    
    * test
    
    * chore: fix profile target
    
    * fix
    
    * fix: use features (memory_allocator)
    
    * test
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>

--- ci/scripts/bench.py
+++ ci/scripts/bench.py
@@ -16,29 +16,23 @@ def run_cargo_command(
     max_segment_length,
     instance_type,
     memory_allocator,
+    output_path,
 ):
     # Command to run
     command = [
         "cargo", "run", "--no-default-features", "--bin", bin_name, "--profile", "maxperf", "--features", ",".join(feature_flags), "--"
     ]
-    output_path = f".bench_metrics/{bin_name}"
 
     if app_log_blowup is not None:
         command.extend(["--app_log_blowup", app_log_blowup])
-        output_path += f"-{app_log_blowup}"
     if agg_log_blowup is not None:
         command.extend(["--agg_log_blowup", agg_log_blowup])
-        output_path += f"-{agg_log_blowup}"
     if root_log_blowup is not None:
         command.extend(["--root_log_blowup", root_log_blowup])
-        output_path += f"-{root_log_blowup}"
     if internal_log_blowup is not None:
         command.extend(["--internal_log_blowup", internal_log_blowup])
-        output_path += f"-{internal_log_blowup}"
     if max_segment_length is not None:
         command.extend(["--max_segment_length", max_segment_length])
-        output_path += f"-{max_segment_length}"
-    output_path += f"-{instance_type}-{memory_allocator}.json"
 
     # Change the current working directory to the Git root
     git_root = get_git_root()
@@ -46,6 +40,7 @@ def run_cargo_command(
     create_bench_metrics_dir()
     output_path_old = None
 
+    # Local only: in CI we will download the old metrics file from S3
     if os.path.exists(output_path):
         output_path_old = f"{output_path}.old"
         shutil.move(output_path, f"{output_path_old}")
@@ -61,6 +56,7 @@ def run_cargo_command(
 
     print(f"Output metrics written to {git_root}/{output_path}")
 
+    # Local only: in CI the old file is not present yet and we will generate markdown in a later step.
     markdown_output = generate_displayable_metrics(output_path, output_path_old)
     with open(f"{git_root}/.bench_metrics/{bin_name}.md", "w") as f:
         f.write(markdown_output)
@@ -77,7 +73,7 @@ def bench():
     parser.add_argument('--internal_log_blowup', type=str, help="Aggregation level log blowup")
     parser.add_argument('--max_segment_length', type=str, help="Max segment length for continuations")
     parser.add_argument('--features', type=str, help="Additional features")
-    parser.add_argument('--flamegraph', type=bool, help="Create flamegraphs")
+    parser.add_argument('--output_path', type=str, required=True, help="The path to write the metrics to")
     args = parser.parse_args()
 
     feature_flags = ["bench-metrics", "parallel", "function-span"] + ([args.features] if args.features else []) + [args.memory_allocator]
@@ -93,6 +89,7 @@ def bench():
         args.max_segment_length,
         args.instance_type,
         args.memory_allocator,
+        args.output_path,
     )
 
 
---
+++ ci/scripts/utils.sh
@@ -0,0 +1,120 @@
+generate_markdown() {
+    local metric_path="$1"
+    local metric_name="$2"
+    local current_sha="$3"
+    local s3_metrics_path="$4"
+    local afs_root="$5"
+
+    if [[ -f $metric_path ]]; then
+        s5cmd cp $metric_path $s3_metrics_path/${current_sha}-${metric_name}.json
+
+        prev_path="${s3_metrics_path}/main-${metric_name}.json"
+        count=`s5cmd ls $prev_path | wc -l`
+
+        if [[ $count -gt 0 ]]; then
+            s5cmd cp $prev_path prev.json
+            python3 ${afs_root}/ci/scripts/metric_unify/main.py $metric_path --prev prev.json --aggregation-json ${afs_root}/ci/scripts/metric_unify/aggregation.json > results.md
+        else
+            echo "No previous benchmark on main branch found"
+            python3 ${afs_root}/ci/scripts/metric_unify/main.py $metric_path --aggregation-json ${afs_root}/ci/scripts/metric_unify/aggregation.json > results.md
+        fi
+    else
+        echo "No benchmark metrics found at ${metric_path}"
+    fi
+}
+
+add_metadata() {
+    local result_path="$1"
+    local max_segment_length="$2"
+    local instance_type="$3"
+    local memory_allocator="$4"
+    local repo="$5"
+    local run_id="$6"
+
+    commit_url="https://github.com/${repo}/commit/${current_sha}"
+    echo "" >> $result_path
+    if [[ "$UPLOAD_FLAMEGRAPHS" == '1' ]]; then
+        echo "<details>" >> $result_path
+        echo "<summary>Flamegraphs</summary>" >> $result_path
+        echo "" >> $result_path
+        for file in .bench_metrics/flamegraphs/*.svg; do
+        filename=$(basename "$file")
+            flamegraph_url=https://axiom-public-data-sandbox-us-east-1.s3.us-east-1.amazonaws.com/benchmark/github/flamegraphs/${current_sha}/${filename}
+            echo "[![]($flamegraph_url)]($flamegraph_url)" >> $result_path
+        done
+        echo "" >> $result_path
+        echo "</details>" >> $result_path
+        echo "" >> $result_path
+    fi
+    echo "Commit: ${commit_url}" >> $result_path
+    echo "" >> $result_path
+    echo "Max Segment Length: $max_segment_length" >> $result_path
+    echo "" >> $result_path
+    echo "Instance Type: $instance_type" >> $result_path
+    echo "" >> $result_path
+    echo "Memory Allocator: $memory_allocator" >> $result_path
+    echo "" >> $result_path
+    echo "[Benchmark Workflow](https://github.com/${repo}/actions/runs/${run_id})" >> $result_path
+    s5cmd cp $result_path "${S3_PATH}/${current_sha}-${METRIC_NAME}.md"
+}
+
+commit_and_push_gh_pages() {
+    local files=$1
+    local commit_message=$2
+    git add ${files}
+    git commit --allow-empty -m "${commit_message}"
+
+    MAX_RETRIES=10
+    RETRY_DELAY=5
+    ATTEMPT=0
+    SUCCESS=false
+
+    while [ $ATTEMPT -lt $MAX_RETRIES ]; do
+        echo "Attempt $((ATTEMPT + 1)) to push of $MAX_RETRIES..."
+        git fetch origin gh-pages
+        git merge origin/gh-pages --no-edit
+        if git push origin gh-pages; then
+            SUCCESS=true
+            break
+        else
+            echo "Push failed. Retrying in $RETRY_DELAY seconds..."
+            sleep $RETRY_DELAY
+            ATTEMPT=$((ATTEMPT + 1))
+        fi
+    done
+
+    if [ "$SUCCESS" = false ]; then
+        echo "PUSH_FAILED"
+        exit 1
+    fi
+}
+
+install_s5cmd() {
+    arch=$(uname -m)
+    case $arch in
+    arm64|aarch64)
+        rustup component add rust-src --toolchain nightly-2024-10-30-aarch64-unknown-linux-gnu
+        S5CMD_BIN="s5cmd_2.2.2_linux_arm64.deb"
+        ;;
+    x86_64|amd64)
+        rustup component add rust-src --toolchain nightly-2024-10-30-x86_64-unknown-linux-gnu
+        S5CMD_BIN="s5cmd_2.2.2_linux_amd64.deb"
+        ;;
+    *)
+        echo "Unsupported architecture: $arch"
+        exit 1
+        ;;
+    esac
+
+    echo "Checking s5cmd"
+    if type s5cmd &>/dev/null; then
+        echo "s5cmd was installed."
+    else
+        TMP_DIR=/tmp/s5cmd
+        rm -rf $TMP_DIR
+        mkdir $TMP_DIR
+        echo "s5cmd was not installed. Installing.."
+        wget "https://github.com/peak/s5cmd/releases/download/v2.2.2/${S5CMD_BIN}" -P $TMP_DIR
+        sudo dpkg -i "${TMP_DIR}/${S5CMD_BIN}"
+    fi
+}
