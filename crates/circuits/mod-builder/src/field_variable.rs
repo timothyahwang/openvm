0aa4ef703f717961d7eaea83c208fe6c8a49390a
Avaneesh-axiom <avaneeshk@intrinsictech.xyz>
2025-02-28 00:08:48 -0500

    [fix] Corrected some assertions in mod-builder (#37)

--- crates/circuits/mod-builder/src/field_variable.rs
+++ crates/circuits/mod-builder/src/field_variable.rs
@@ -333,21 +333,15 @@ impl FieldVariable {
 
     pub fn select(flag_id: usize, a: &FieldVariable, b: &FieldVariable) -> FieldVariable {
         assert!(Rc::ptr_eq(&a.builder, &b.builder));
-        let left_limb_max_abs = max(a.limb_max_abs, b.limb_max_abs);
-        let left_max_overflow_bits = max(a.max_overflow_bits, b.max_overflow_bits);
-        let left_expr_limbs = max(a.expr_limbs, b.expr_limbs);
-        let right_limb_max_abs = left_limb_max_abs;
-        let right_max_overflow_bits = left_max_overflow_bits;
-        let right_expr_limbs = left_expr_limbs;
-        assert_eq!(left_limb_max_abs, right_limb_max_abs);
-        assert_eq!(left_max_overflow_bits, right_max_overflow_bits);
-        assert_eq!(left_expr_limbs, right_expr_limbs);
+        let limb_max_abs = max(a.limb_max_abs, b.limb_max_abs);
+        let max_overflow_bits = max(a.max_overflow_bits, b.max_overflow_bits);
+        let expr_limbs = max(a.expr_limbs, b.expr_limbs);
         FieldVariable {
             expr: SymbolicExpr::Select(flag_id, Box::new(a.expr.clone()), Box::new(b.expr.clone())),
             builder: a.builder.clone(),
-            limb_max_abs: left_limb_max_abs,
-            max_overflow_bits: left_max_overflow_bits,
-            expr_limbs: left_expr_limbs,
+            limb_max_abs,
+            max_overflow_bits,
+            expr_limbs,
             range_checker_bits: a.range_checker_bits,
         }
     }
