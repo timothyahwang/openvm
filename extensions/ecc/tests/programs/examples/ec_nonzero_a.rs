673c67cd3e7cd201d436217c92558b0d1b15441c
Avaneesh-axiom <avaneeshk@intrinsictech.xyz>
2025-03-28 01:50:31 -0400

    fix: Add p256 feature to clippy when linting ecc test guest programs (#1528)
    The linter did not lint some guest programs in the ecc tests because the
    `p256` feature was not enabled.

--- extensions/ecc/tests/programs/examples/ec_nonzero_a.rs
+++ extensions/ecc/tests/programs/examples/ec_nonzero_a.rs
@@ -1,13 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_main)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use core::hint::black_box;
-
 use hex_literal::hex;
 use openvm_algebra_guest::IntMod;
 use openvm_ecc_guest::{
-    msm,
-    p256::{P256Coord, P256Point, P256Scalar},
+    p256::{P256Coord, P256Point},
     weierstrass::WeierstrassPoint,
     CyclicGroup, Group,
 };
@@ -57,7 +54,7 @@ pub fn main() {
 
     // Test generator
     let (gen_x, gen_y) = P256Point::GENERATOR.into_coords();
-    let generator = P256Point::from_xy(gen_x, gen_y).unwrap();
+    let _generator = P256Point::from_xy(gen_x, gen_y).unwrap();
     let (neg_x, neg_y) = P256Point::NEG_GENERATOR.into_coords();
-    let neg_generator = P256Point::from_xy(neg_x, neg_y).unwrap();
+    let _neg_generator = P256Point::from_xy(neg_x, neg_y).unwrap();
 }
