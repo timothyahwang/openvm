63ec17fe01c0214610fa67e44b3851976215fdfe
Yi Sun <yi-sun@users.noreply.github.com>
2025-05-23 15:13:58 -0400

    fix: change parallel estimate to account for serial execution (#1686)
    Closes INT-3770

--- crates/prof/src/aggregate.rs
+++ crates/prof/src/aggregate.rs
@@ -165,6 +165,7 @@ impl AggregateMetrics {
         let mut total_par_proof_time = MdTableCell::new(0.0, Some(0.0));
         for (group_name, metrics) in &self.by_group {
             let stats = metrics.get(PROOF_TIME_LABEL);
+            let execute_stats = metrics.get(EXECUTE_TIME_LABEL);
             if stats.is_none() {
                 continue;
             }
@@ -186,6 +187,24 @@ impl AggregateMetrics {
                 *total_proof_time.diff.as_mut().unwrap() += sum.diff.unwrap_or(0.0);
                 total_par_proof_time.val += max.val;
                 *total_par_proof_time.diff.as_mut().unwrap() += max.diff.unwrap_or(0.0);
+
+                // Account for the fact that execution is serial
+                // Add total execution time for the app proofs, and subtract the max segment
+                // execution time
+                if group_name != "leaf"
+                    && group_name != "root"
+                    && group_name != "halo2_outer"
+                    && group_name != "halo2_wrapper"
+                    && !group_name.starts_with("internal")
+                {
+                    let execute_stats = execute_stats.unwrap();
+                    total_par_proof_time.val +=
+                        (execute_stats.sum.val - execute_stats.max.val) / 1000.0;
+                    *total_par_proof_time.diff.as_mut().unwrap() +=
+                        (execute_stats.sum.diff.unwrap_or(0.0)
+                            - execute_stats.max.diff.unwrap_or(0.0))
+                            / 1000.0;
+                }
             }
         }
         self.total_proof_time = total_proof_time;
