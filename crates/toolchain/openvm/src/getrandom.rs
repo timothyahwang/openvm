3c800070d363237832a66dbe5501d3c365f3c549
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-06-27 09:59:09 -0700

    fix: `getrandom` pulling in `std` (#1804)
    For reasons I am not entirely clear on, when you use
    `getrandom_v02::Error`, the `getrandom` crate pulls in `std` library.
    But if you just directly implement what the `register_custom_getrandom!`
    macro does, this goes away...
    
    Fixes CI failure.
    
    Full disclosure: I got the idea from
    https://github.com/risc0/risc0/pull/3106/files#diff-fd010fc66ba8d918981d2705c0d10cd65d19db599969bb29bb14caffa62d3b65R95

--- crates/toolchain/openvm/src/getrandom.rs
+++ crates/toolchain/openvm/src/getrandom.rs
@@ -13,10 +13,14 @@ unsafe extern "Rust" fn __getrandom_v03_custom(
     Err(getrandom::Error::UNSUPPORTED)
 }
 
+/// This entrypoint for getrandom is used for versions < 0.3
+// The ABI is defined here: https://github.com/rust-random/getrandom/blob/ce4144b2c16fe1422037c93e267e6a52336e0834/src/custom.rs#L74
+// @dev If you try to use the `getrandom_v02::Error`, it somehow triggers std library
 #[cfg(feature = "getrandom-unsupported")]
-pub fn __getrandom_v02_custom(_dest: &mut [u8]) -> Result<(), getrandom_v02::Error> {
-    Err(getrandom_v02::Error::UNSUPPORTED)
+#[no_mangle]
+unsafe fn __getrandom_custom(dest: *mut u8, len: usize) -> u32 {
+    __getrandom_v03_custom(dest, len)
+        .map_err(|e| e.raw_os_error().unwrap_or(2))
+        .err()
+        .unwrap_or(0) as u32
 }
-// https://docs.rs/getrandom/0.2.16/src/getrandom/custom.rs.html#74
-#[cfg(feature = "getrandom-unsupported")]
-getrandom_v02::register_custom_getrandom!(__getrandom_v02_custom);
